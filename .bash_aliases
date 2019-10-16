6e5d61325c21893aa276d5954f26fa17a08c124d
florian <flrn.pjd@protonmail.com>
2019-10-16 23:47:47 +0200

    linked hosts, added cl alias, added seclists clone to parrot_install.sh

--- .bash_aliases
+++ .bash_aliases
@@ -4,6 +4,7 @@ alias gdbui='gdb -tui --nx -q'
 alias pudb='python3 -m pudb'
 alias vg='valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes'
 alias open='xdg-open'
+alias cl='clear'
 
 cutter () { /home/florian/Documents/Cutter-v1.8.3-x64.Linux.AppImage "$1" &> /dev/null & }
 
--- parrot_install.sh
+++ parrot_install.sh
@@ -3,7 +3,13 @@
 cur=/home/florian/.dotfiles
 home_florian=/home/florian
 
-sudo apt update && sudo apt upgrade
+# TO DO BEFORE RUNNING THIS!
+# sudo apt update && sudo apt upgrade
+# git config --global user.name "florian"
+# git config --global user.email flrn.pjd@protonmail.com
+# ssh-keygen -t rsa -b 4096 -C "florian@parrot.local"
+# ssh-add ~/.ssh/id_rsa
+
 mkdir -p $home_florian/.config/i3
 mkdir -p $home_florian/.config/i3status
 rm $home_florian/.config/i3/config
@@ -16,7 +22,6 @@ gsettings set org.mate.interface gtk-decoration-layout ""
 rm $home_florian/.vimrc
 ln $cur/.vimrc $home_florian/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git $home_florian/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
 rm $home_florian/.tmux.conf
 ln $cur/.tmux.conf $home_florian/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm $home_florian/.tmux/plugins/tpm
@@ -35,12 +40,15 @@ rm $home_florian/.gdbinit
 ln $cur/.gdbinit $home_florian/.gdbinit
 sudo apt remove neovim
 sudo apt install tmux i3 ranger python3-dev py3status fonts-powerline rlwrap ncat vim mingw-w64 exploitdb python3-pyftpdlib markdown python-xlrd rdesktop nfs-common chromium
+vim +PluginInstall +qall
 sudo pip2 install pwntools
 sudo pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
 git clone https://github.com/pwndbg/pwndbg $home_florian/pwndbg
 cd $home_florian/pwndbg
 $home_florian/pwndbg/setup.sh
 sudo updatedb
-ssh-keygen -t rsa -b 4096 -C "florian@parrot"
-ssh-add ~/.ssh/id_rsa
+sudo rm /etc/hosts
+sudo ln $cur/hosts /etc/hosts
+git clone https://github.com/danielmiessler/SecLists.git $home_florian/seclists
+sudo mv $home_florian/seclists /usr/share/seclists
 echo "Reboot now, don't forget to add your public key to blackarch's authorized_keys"
